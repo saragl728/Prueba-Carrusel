a7e615eb96cd1d102c7874b30bd91180fefc441d
saragl728 <saragl728@gmail.com>
2024-12-03 18:21:26 +0100

    Añado variables de elementos html al script y paso funciones

--- escript.js
+++ escript.js
@@ -11,28 +11,65 @@ var indx = -1; //variable para rotar en order, lo pongo a -1 para que cuando emp
 
 var rotando = false; //usaremos esto para la rotación
 
+//elementos html
+var cuerpo = document.getElementsByTagName("body")[0];
+var imag = document.getElementById("imagen");
+var estad = document.getElementById("estado");
+var titulo = document.getElementById("tit");
+var deten = document.getElementById("para");
+var conti = document.getElementById("sigue");
+var maas = document.getElementById("mas");
+
 switch (navigator.language) {
   case "es-ES":
   case "es-AR":
   default:
-    document
-      .getElementById("imagen")
-      .setAttribute("alt", "Fondo de pantalla incluido en Windows 7");
-    document.getElementById("tit").innerHTML = "Imagen cambiante";
-    document.getElementById("para").title = "Para";
-    document.getElementById("sigue").title = "Continúa";
-    document.getElementById("mas").innerHTML =
+    imag.setAttribute("alt", "Fondo de pantalla incluido en Windows 7");
+    titulo.innerHTML = "Imagen cambiante";
+    deten.title = "Para";
+    conti.title = "Continúa";
+    maas.innerHTML =
       "Haz doble click en la imagen para detener o continuar el pase de imágenes";
     break;
   case "en-EN":
   case "en-US":
-    document
-      .getElementById("imagen")
-      .setAttribute("alt", "Wallpaper included in Windows 7");
-    document.getElementById("tit").innerHTML = "Changing image";
-    document.getElementById("para").title = "Stop";
-    document.getElementById("sigue").title = "Continue";
-    document.getElementById("mas").innerHTML =
-      "Double click the imagen to stop or continue images slide";
+    imag.setAttribute("alt", "Wallpaper included in Windows 7");
+    titulo.innerHTML = "Changing image";
+    deten.title = "Stop";
+    conti.title = "Continue";
+    maas.innerHTML = "Double click the imagen to stop or continue images slide";
     break;
 }
+
+//funciones
+function estableceImagen(fuente) {
+  imag.src = fuente;
+}
+
+function mensajeRotando() {
+  switch (navigator.language) {
+    case "es-ES":
+    case "es-AR":
+    default:
+      estad.textContent = "Rotando...";
+      break;
+    case "en-EN":
+    case "en-US":
+      estad.textContent = "Rotating...";
+      break;
+  }
+}
+
+function rotaAlAzar() {
+  let ind = Math.floor(Math.random() * imagenes.length); //esta variable es para obtener el índice del array
+
+  estableceImagen(imagenes[ind]); //le damos la fuente a la imagen
+
+  mensajeRotando();
+
+  if (!rotando) rotando = true;
+  //se cambia el estilo para que quede claro que está activo
+  if (estad.className != "activo") estad.className = "activo";
+}
+
+//asignación de eventos
